4ded972652a1821843dca98c8c744cb65378df68
Heitor <heitorvieirafernandes12@gmail.com>
2025-11-11 10:08:59 -0300

    renomeando controller

--- CadastroDeProdutos/CadastroDeProdutos/Repositorio/ProdutoRepositorio.cs
+++ CadastroDeProdutos/CadastroDeProdutos/Repositorio/ProdutoRepositorio.cs
@@ -8,7 +8,6 @@ namespace CadastroDeProdutos.Repositorio
 {
     public class ProdutoRepositorio(IConfiguration configuration)
     {
-        // Declara um campo privado somente leitura para armazenar a string de conexão com o MySQL.
         private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
 
         public void AdicionarProduto(Produto produto)
@@ -17,7 +16,6 @@ namespace CadastroDeProdutos.Repositorio
             {
                 conexao.Open();
                 var cmd = conexao.CreateCommand();
-                // Cria um novo comando SQL para inserir dados na tabela 'cliente'
                 cmd.CommandText = "INSERT INTO produto (Nome,Descricao,Preco,Quantidade) VALUES (@nome, @descricao, @preco, @quantidade)";
 
                 cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = produto.Nome;
@@ -25,154 +23,105 @@ namespace CadastroDeProdutos.Repositorio
                 cmd.Parameters.Add("@preco", MySqlDbType.Decimal).Value = produto.Preco;
                 cmd.Parameters.Add("@quantidade", MySqlDbType.Decimal).Value = produto.Quantidade;
 
-                // Executa o comando SQL de inserção e retorna o número de linhas afetadas
                 cmd.ExecuteNonQuery();
-                // Fecha explicitamente a conexão com o banco de dados 
                 conexao.Close();
 
             }
         }
 
-        // Método para Editar (atualizar) os dados de um cliente existente no banco de dados
         public bool Atualizar(Produto produto)
         {
             try
             {
-                // Bloco using para garantir que a conexão seja fechada e os recursos liberados após o uso
                 using (var conexao = new MySqlConnection(_conexaoMySQL))
                 {
-                    // Abre a conexão com o banco de dados MySQL
                     conexao.Open();
-                    // Cria um novo comando SQL para atualizar dados na tabela 'cliente' com base no código
                     MySqlCommand cmd = new MySqlCommand("Update produto set Nome=@nome, Descricacao=@descricao, Preco=@preco, Quantidade=@quantidade " + " where id=@codigo ", conexao);
-                    // Adiciona um parâmetro para o código do cliente a ser atualizado, definindo seu tipo e valor
                     cmd.Parameters.Add("@codigo", MySqlDbType.Int32).Value = produto.id;
-                    // Adiciona um parâmetro para o novo nome, definindo seu tipo e valor
                     cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = produto.Nome;
-                    // Adiciona um parâmetro para o novo telefone, definindo seu tipo e valor
                     cmd.Parameters.Add("@descricao", MySqlDbType.VarChar).Value = produto.Descricao;
-                    // Adiciona um parâmetro para o novo email, definindo seu tipo e valor
                     cmd.Parameters.Add("@preco", MySqlDbType.Decimal).Value = produto.Preco;
                     cmd.Parameters.Add("@quantidade", MySqlDbType.Decimal).Value = produto.Quantidade;
-                    // Executa o comando SQL de atualização e retorna o número de linhas afetadas
-                    //executa e verifica se a alteração foi realizada
                     int linhasAfetadas = cmd.ExecuteNonQuery();
-                    return linhasAfetadas > 0; // Retorna true se ao menos uma linha foi atualizada
+                    return linhasAfetadas > 0;
 
                 }
             }
             catch (MySqlException ex)
             {
-                // Logar a exceção (usar um framework de logging como NLog ou Serilog)
                 Console.WriteLine($"Erro ao atualizar produto: {ex.Message}");
-                return false; // Retorna false em caso de erro
+                return false;
 
             }
         }
 
-        // Método para listar todos os clientes do banco de dados
         public IEnumerable<Produto> TodosProdutos()
         {
-            // Cria uma nova lista para armazenar os objetos Cliente
             List<Produto> Produtolist = new List<Produto>();
 
-            // Bloco using para garantir que a conexão seja fechada e os recursos liberados após o uso
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
-                // Abre a conexão com o banco de dados MySQL
                 conexao.Open();
-                // Cria um novo comando SQL para selecionar todos os registros da tabela 'cliente'
                 MySqlCommand cmd = new MySqlCommand("SELECT * from produto", conexao);
 
-                // Cria um adaptador de dados para preencher um DataTable com os resultados da consulta
                 MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                // Cria um novo DataTable
                 DataTable dt = new DataTable();
-                // metodo fill- Preenche o DataTable com os dados retornados pela consulta
                 da.Fill(dt);
-                // Fecha explicitamente a conexão com o banco de dados 
                 conexao.Close();
 
-                // interage sobre cada linha (DataRow) do DataTable
                 foreach (DataRow dr in dt.Rows)
                 {
-                    // Cria um novo objeto Cliente e preenche suas propriedades com os valores da linha atual
                     Produtolist.Add(
                                 new Produto
                                 {
-                                    id = Convert.ToInt32(dr["id"]), // Converte o valor da coluna "codigo" para inteiro
-                                    Nome = ((string)dr["Nome"]), // Converte o valor da coluna "nome" para string
-                                    Descricao = ((string)dr["Descricacao"]), // Converte o valor da coluna "telefone" para string
+                                    id = Convert.ToInt32(dr["id"]), 
+                                    Nome = ((string)dr["Nome"]),
+                                    Descricao = ((string)dr["Descricacao"]),
                                     Preco = ((decimal)dr["Preco"]),
-                                    Quantidade = ((decimal)dr["Quantidade"]),// Converte o valor da coluna "email" para string
+                                    Quantidade = ((decimal)dr["Quantidade"]),
                                 });
                 }
-                // Retorna a lista de todos os clientes
                 return Produtolist;
             }
         }
 
-        // Método para buscar um cliente específico pelo seu código (Codigo)
         public Produto ObterProduto(int Codigo)
         {
-            // Bloco using para garantir que a conexão seja fechada e os recursos liberados após o uso
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
-                // Abre a conexão com o banco de dados MySQL
                 conexao.Open();
-                // Cria um novo comando SQL para selecionar um registro da tabela 'cliente' com base no código
                 MySqlCommand cmd = new MySqlCommand("SELECT * from produto where id=@codigo ", conexao);
-
-                // Adiciona um parâmetro para o código a ser buscado, definindo seu tipo e valor
                 cmd.Parameters.AddWithValue("@codigo", Codigo);
-
-                // Cria um adaptador de dados (não utilizado diretamente para ExecuteReader)
                 MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-
-                // Declara um leitor de dados do MySQL
                 MySqlDataReader dr;
-                // Cria um novo objeto Cliente para armazenar os resultados
                 Produto produto = new Produto();
 
-                /* Executa o comando SQL e retorna um objeto MySqlDataReader para ler os resultados
-                CommandBehavior.CloseConnection garante que a conexão seja fechada quando o DataReader for fechado*/
-
                 dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
-                // Lê os resultados linha por linha
                 while (dr.Read())
                 {
-                    // Preenche as propriedades do objeto Cliente com os valores da linha atual
-                    produto.id = Convert.ToInt32(dr["id"]);//propriedade Codigo e convertendo para int
-                    produto.Nome = (string)(dr["Nome"]); // propriedade Nome e passando string
-                    produto.Descricao = (string)(dr["Descricao"]); //propriedade telefone e passando string
-                    produto.Preco= (decimal)(dr["Preco"]); //propriedade email e passando string
+                    produto.id = Convert.ToInt32(dr["id"]);
+                    produto.Nome = (string)(dr["Nome"]);
+                    produto.Descricao = (string)(dr["Descricao"]);
+                    produto.Preco= (decimal)(dr["Preco"]);
                     produto.Quantidade = (decimal)(dr["Quantidade"]);
                 }
-                // Retorna o objeto Cliente encontrado (ou um objeto com valores padrão se não encontrado)
                 return produto;
             }
         }
 
-
-        // Método para excluir um cliente do banco de dados pelo seu código (ID)
         public void Excluir(int Id)
         {
-            // Bloco using para garantir que a conexão seja fechada e os recursos liberados após o uso
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
-                // Abre a conexão com o banco de dados MySQL
                 conexao.Open();
 
-                // Cria um novo comando SQL para deletar um registro da tabela 'cliente' com base no código
                 MySqlCommand cmd = new MySqlCommand("delete from cliente where CodCli=@codigo", conexao);
 
-                // Adiciona um parâmetro para o código a ser excluído, definindo seu tipo e valor
                 cmd.Parameters.AddWithValue("@codigo", Id);
 
-                // Executa o comando SQL de exclusão e retorna o número de linhas afetadas
                 int i = cmd.ExecuteNonQuery();
 
-                conexao.Close(); // Fecha  a conexão com o banco de dados
+                conexao.Close();
             }
         }
     }
